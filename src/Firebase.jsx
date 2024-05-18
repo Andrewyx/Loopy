d7067f3a7b8421cd5fb7152bed240e4e58b819a5
Andrew Mao <andrewyxmao@gmail.com>
2024-05-18 13:57:50 -0700

    basic firebase config

---
+++ src/Firebase.jsx
@@ -0,0 +1,74 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
+
+/**
+ * Singleton for all Firebase-related tasks and information
+ */
+
+export default class Firebase {
+    static #instance;
+    #app;
+    #auth;
+    #db;
+    #firebaseConfig = {
+        apiKey: "AIzaSyBm7QmFuGXrCH8PGJQoXCtbEP_02UKOPXg",
+        authDomain: "loopy-423720.firebaseapp.com",
+        projectId: "loopy-423720",
+        storageBucket: "loopy-423720.appspot.com",
+        messagingSenderId: "386160874263",
+        appId: "1:386160874263:web:8900e555007196181d1d52"
+    };
+    
+
+    constructor() {
+        this.app = initializeApp(firebaseConfig);
+        this.db = getFirestore(app);
+        this.auth = getAuth();   
+    }
+
+    /**
+     * Instantiates the Firebase object or gets the single instance of it
+     */
+    static getInstance() {
+        if (this.#instance) {
+            return this.#instance;
+        }
+        else {
+            this.#instance = new Firebase();
+            return this.#instance; 
+        }
+    }
+
+    /**
+     * Returns true if Firebase object exists, otherwise false
+     * 
+     * @returns boolean
+     */
+    static isInstaniated() {
+        if (this.#instance) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    /**
+     * Getter for Firebase auth instance
+     * 
+     * @returns Auth for Firebase Instance
+     */
+    getAuth() {
+        return this.#auth;
+    }
+
+    /**
+     * Getter for Cloud Firestore Database
+     * 
+     * @returns Firestore Database
+     */
+    getDatabase() {
+        return this.#db;
+    }
+}
